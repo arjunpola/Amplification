8ed2a49fb1a33086cafea3f4c5169158dffeddb5
Nagarjun Pola <arjun.pola@gmail.com>
2023-02-18 10:54:09 +0530

    Create a custom API

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -1,11 +1,13 @@
 import * as common from "@nestjs/common";
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver, Context } from "@nestjs/graphql";
+import { Request } from "express";
 import * as gqlACGuard from "../auth/gqlAC.guard";
 import { AuthService } from "./auth.service";
 import { GqlDefaultAuthGuard } from "./gqlDefaultAuth.guard";
 import { UserData } from "./userData.decorator";
 import { LoginArgs } from "./LoginArgs";
 import { UserInfo } from "./UserInfo";
+import { User } from "../user/base/User";
 
 @Resolver(UserInfo)
 export class AuthResolver {
@@ -19,5 +21,15 @@ export class AuthResolver {
   @common.UseGuards(GqlDefaultAuthGuard, gqlACGuard.GqlACGuard)
   async userInfo(@UserData() userInfo: UserInfo): Promise<UserInfo> {
     return userInfo;
+  }
+
+  @Mutation(() => UserInfo)
+  async signup(@Args() args: LoginArgs): Promise<UserInfo> {
+    return this.authService.signup(args.credentials);
+  }
+
+  @Query(() => User)
+  async me(@Context("req") request: Request): Promise<User> {
+    return this.authService.me(request.headers.authorization);
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  Injectable,
+  UnauthorizedException,
+  NotFoundException,
+} from "@nestjs/common";
 // @ts-ignore
 // eslint-disable-next-line
 import { UserService } from "../user/user.service";
 import { Credentials } from "./Credentials";
 import { PasswordService } from "./password.service";
 import { TokenService } from "./token.service";
+import { User } from "../user/base/User";
 import { UserInfo } from "./UserInfo";
 
 @Injectable()
@@ -48,5 +53,55 @@ export class AuthService {
       accessToken,
       ...user,
     };
+  }
+  async signup(credentials: Credentials): Promise<UserInfo> {
+    // Extract the username and password from the body of the request
+    const { username, password } = credentials;
+    // Here we attempt to create a new user
+    const user = await this.userService.create({
+      data: {
+        username,
+        password,
+        roles: ["todoUser"], // Here we assign every new user the `Todo User` role
+      },
+    });
+    // If creating a new user fails throw an error
+    if (!user) {
+      throw new UnauthorizedException("Could not create user");
+    }
+    // Create an access token for the newly created user
+    const accessToken = await this.tokenService.createToken({
+      id: user.id,
+      username,
+      password,
+    });
+    // Return the access token as well as the some details about the user
+    return {
+      accessToken,
+      username: user.username,
+      id: user.id,
+      roles: user.roles as string[],
+    };
+  }
+  async me(authorization: string = ""): Promise<User> {
+    const bearer = authorization.replace(/^Bearer\s/, "");
+    const username = this.tokenService.decodeToken(bearer);
+    const result = await this.userService.findOne({
+      where: { username },
+      select: {
+        createdAt: true,
+        firstName: true,
+        id: true,
+        lastName: true,
+        roles: true,
+        updatedAt: true,
+        username: true,
+      },
+    });
+    if (!result) {
+      throw new NotFoundException(`No resource was found for ${username}`);
+    }
+
+    return result;
   }
 }
